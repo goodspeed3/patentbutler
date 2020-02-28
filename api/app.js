0b0d0a0742b2c655c2edb2b8bbb7490cead4353d
Jon Liu <jon.liu11@gmail.com>
2020-02-27 20:56:04 -0800

    solved some routes, hopefullY?

--- api/app.js
+++ api/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,9 +18,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('../client/build'));
+// -app.get('/', function (req, res) {
+//   +app.get('/*', function (req, res) {
+//     res.sendFile('../client/build/index.html');
+//   });
+// });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve('../client/build/index.html'));
+})
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
